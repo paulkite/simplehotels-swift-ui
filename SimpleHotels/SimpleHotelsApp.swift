7b7f35cfe8fe4e8b0d7956f0a28376f7085c3402
Paul Kite <paulkite@mac.com>
2023-11-22 17:26:26 -0800

    Initial project commit.

---
+++ SimpleHotels/SimpleHotelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleHotelsApp.swift
+//  SimpleHotels
+//
+//  Created by Paul Kite on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleHotelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
